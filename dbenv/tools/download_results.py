ba19e4398ed30c780deb3a4016cc6b4c7208ce42
Yuma Iwasaki <requrd1989@gmail.com>
2018-10-12 23:48:47 +0900

    ダウンロードモジュール追加

---
+++ dbenv/tools/download_results.py
@@ -0,0 +1,25 @@
+import requests,os,sys,zipfile
+from datetime import datetime
+ymd = sys.args[1]
+year = ymd[0:4]
+date = ymd[4:8] 
+
+#JRDB会員情報
+user = '18045423'
+pwd = '01102743'
+
+#前日データ取得処理
+filecode = "SED"
+filename = filecode + year[2:4] + date + ".zip"
+url = "http://www.jrdb.com/member/datazip/" + filecode.title() + "/" + year + "/" + filename
+DOWNLOAD_SAVE_DIR = "./"
+response = requests.get(url,auth=(user, pwd))
+saveFileName = filename
+saveFilePath = os.path.join(DOWNLOAD_SAVE_DIR, saveFileName)
+
+with open(saveFilePath, 'wb') as saveFile:
+    saveFile.write(response.content)
+
+#解凍処理
+with zipfile.ZipFile(filename) as existing_zip:
+    existing_zip.extractall('./')
