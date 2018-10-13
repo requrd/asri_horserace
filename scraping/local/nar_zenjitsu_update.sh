dcb333160afcb2f4f9e53e579c94698a30441b85
Yuma Iwasaki <requrd1989@gmail.com>
2018-10-13 19:17:18 +0900

    クーロン用環境変数修正

---
+++ scraping/local/nar_zenjitsu_update.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+###環境変数設定###
+###bashrcに記載の内容を転記###
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+export PATH="$PYENV_ROOT/versions/miniconda3-4.3.30/bin/:$PATH"
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+. /home/requrd/.pyenv/versions/miniconda3-4.3.30/etc/profile.d/conda.sh
+
+###DB,モジュール保存場所###
+dbenv='/mnt/horseracing/predict/dbenv/NAR'
+git_lib='/mnt/horseracing/predict/git_lib/scraping/local'
+
+###処理開始###
+conda activate tensorflow #minicondaの環境で実行
+ymd=$1
+echo 'バックアップ開始'
+cp $dbenv/kichiuma.db $dbenv/kichiuma_${ymd}_input_bk.db
+echo 'バックアップ完了!'
+mkdir 'work'
+cd ./work
+echo `pwd`
+echo 'モジュール類のコピー開始'
+echo 'DBファイルコピー開始'
+cp $dbenv/kichiuma.db ./
+echo 'DBファイルコピー完了'
+echo 'モジュールコピー開始'
+cp $git_lib/kichiuma_setup.py ./
+cp $git_lib/courseutil.py ./
+cp $git_lib/add_input.py ./
+echo 'モジュールコピー完了'
+echo '更新処理開始'
+python add_input.py $ymd
+echo '更新完了'
+mv kichiuma.db $dbenv
+cd ..
+rm -R ./work
+echo 'jrdb.db moved!'
