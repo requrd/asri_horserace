5e604ce3bb23c2779ac1d5dfe94d81eb2def2898
Yuma Iwasaki <requrd1989@gmail.com>
2019-08-02 00:09:41 +0900

    Create schedule

---
+++ JRA/util/schedule.py
@@ -0,0 +1,32 @@
+import requests
+import re
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+YEAR = int(datetime.now().strftime("%Y"))
+
+def annual_schedule(year=YEAR):
+    days = []
+    for month in range(1,13):
+        for day in open_days(month):
+            days.append("{:04d}{:02d}{:02d}".format(year, month, day))
+    return days
+
+
+def open_days(month, year=YEAR):
+    base_url = "https://keiba.yahoo.co.jp/schedule/list/{}".format(
+        year)
+    payload = {"month": str(month)}
+    response = requests.get(base_url, payload)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return _parse_to_days(soup)
+
+
+def _parse_to_days(soup):
+    days = []
+    for s in soup.find("table").find_all("tr"):
+        if s.td is not None:
+            day = re.sub("\\D", "", s.td.contents[0])
+            if day != "":
+                days.append(int(day))
+    return list(set(days))
