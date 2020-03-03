39b612426dca371be7930bca008064489113a9d7
Yuma Iwasaki <requrd1989@gmail.com>
2020-03-03 23:41:27 +0900

    Apply black

--- JRA/util/jrdbdummies.py
+++ JRA/util/jrdbdummies.py
@@ -53,8 +53,8 @@ class CategoryGetter:
         return self.categories(5, s)
 
     def getJoken(self, value):
-        types = ["04","05","08","09","10","15","16","A1","A2","A3","OP"]
-        return self.categories(11, types.index(value)+1)
+        types = ["04", "05", "08", "09", "10", "15", "16", "A1", "A2", "A3", "OP"]
+        return self.categories(11, types.index(value) + 1)
 
     def getJuryo(self, j):
         return self.categories(4, j)
