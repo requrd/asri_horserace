512dd4251a5889b1195db6d5ba84011dc13afe4f
Yuma Iwasaki <requrd1989@gmail.com>
2020-03-08 12:03:43 +0900

    Fix category getter

--- JRA/util/category_data.py
+++ JRA/util/category_data.py
@@ -1,54 +1,49 @@
 from jrdbdummies import CategoryGetter
-cg = CategoryGetter()
+import numpy as np
+import training_tool
 
-wnr = 0
-w = 0
-cdw = []
-for i in range(len(kaisais)):
-    w_kaisai = kaisais[i]
-    for j in range(len(w_kaisai.races)):
-        w_race = w_kaisai.races[j]
-        for k in range (len(w_race.racehorses)):
-            w_horse = w_race.racehorses[k]
-            w_hdummies = np.zeros([0])
-            
-            #開催データ
-            w_tennatsu=cg.getTennatsu(w_kaisai.tennatsu)
-            
-            #レースデータ
-            w_distance=cg.getDistance(w_race.distance)
-            
-            #競走馬データ
-            w_bacode=cg.getBacode(w_horse.bacode)
-            w_num=cg.getNum(w_horse.num)            
-            w_waku=cg.getWaku(w_horse.waku)            
-            w_torikeshi=cg.getTorikeshi(w_horse.torikeshi)
-            w_banushikai_code=cg.getBanushikaicode(w_horse.banushikai_code)
-            w_train_type=cg.getTraintype(w_horse.trainanalysis.train_type)
-            
-            w_hdummies = np.hstack((
-                       w_tennatsu,
+def getCategoryData(kaisais):
+    cg = CategoryGetter()
+    cdw = []
+    for kaisai in kaisais:
+        for race in kaisai.races:
+            for horse in race.racehorses:
+                w_hdummies = np.zeros([0])
+                
+                #開催データ
+                w_tennatsu=cg.getTennatsu(kaisai.tennatsu)
+                
+                #レースデータ
+                w_distance=cg.getDistance(race.distance)
+                
+                #競走馬データ
+                w_bacode=cg.getBacode(horse.bacode)
+                w_num=cg.getNum(horse.num)            
+                w_waku=cg.getWaku(horse.waku)            
+                w_torikeshi=cg.getTorikeshi(horse.torikeshi)
+                w_banushikai_code=cg.getBanushikaicode(horse.banushikai_code)
+                w_train_type=cg.getTraintype(horse.trainanalysis.train_type)
+                
+                w_hdummies = np.hstack((
+                        w_tennatsu,
+                        w_distance,
+                        w_bacode,
+                        w_num,
+                        w_torikeshi,
+                        w_banushikai_code,
+                        w_train_type
+                        ))
+                if horse.num - 1  == 0:
+                    w_vdummies = w_hdummies
+                else :
+                    w_vdummies = np.vstack((w_vdummies,w_hdummies))
+            cdw.append(w_vdummies)
+    num_race = training_tool.numberOfRaces(kaisais)
+    num_max_horse = 18
+    category_data = np.zeros([num_race,num_max_horse,len(cdw[0][0])])
 
-                       w_distance,
+    for i in range(num_race):
+        for j in range(len(cdw[i])):
+            category_data[i][j] = cdw[i][j]
 
-                       w_bacode,
-                       w_num,
-                       w_torikeshi,
-                       w_banushikai_code,
-                       w_train_type
-                      ))
-            if k == 0:
-                w_vdummies = w_hdummies
-            else :
-                w_vdummies = np.vstack((w_vdummies,w_hdummies))
-            w += 1
-        cdw.append(w_vdummies)
-        wnr += 1
-    
-category_data = np.zeros([num_race,num_max_horse,len(cdw[0][0])])
-
-for i in range(num_race):
-    for j in range(len(cdw[i])):
-        category_data[i][j] = cdw[i][j]
-
-return category_data
+    return category_data
