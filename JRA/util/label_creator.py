4af464de37bf390331fabf0acc4ceb4af3dd36cf
Yuma Iwasaki <requrd1989@gmail.com>
2020-03-08 16:48:31 +0900

    Add label creator  class

--- JRA/util/label_creator.py
+++ JRA/util/label_creator.py
@@ -1,25 +1,28 @@
 ﻿import numpy as np
+import training_tool
 
-#ラベルデータ作成
-t_icchaku = np.zeros([num_race,num_max_horse])
-t_nichaku = np.zeros([num_race,num_max_horse])
-t_sanchaku = np.zeros([num_race,num_max_horse])
-t_yonchaku = np.zeros([num_race,num_max_horse])
-t_gochaku = np.zeros([num_race,num_max_horse])
-
-w_num = 0
-for kaisai in kaisais:
-    for race in kaisai.races:
-        for horse in race.racehorses:
-            oa = horse.result.order_of_arrival            
-            if oa == 1:            
-                t_icchaku[w_num,horse.num - 1] = 1
-            elif oa == 2:
-                t_nichaku[w_num,horse.num - 1] = 1
-            elif oa == 3:
-                t_sanchaku[w_num,horse.num - 1] = 1
-            elif oa == 4:
-                t_yonchaku[w_num,horse.num - 1] = 1
-            elif oa == 5:
-                t_gochaku[w_num,horse.num - 1] = 1        
-        w_num += 1
+class LabelCreator:
+    def __init__(self, kaisai):
+        num_race = training_tool.numberOfRaces(kaisai)
+        num_max_horse = 18
+        self.t_icchaku = np.zeros([num_race,num_max_horse])
+        self.t_nichaku = np.zeros([num_race,num_max_horse])
+        self.t_sanchaku = np.zeros([num_race,num_max_horse])
+        self.t_yonchaku = np.zeros([num_race,num_max_horse])
+        self.t_gochaku = np.zeros([num_race,num_max_horse])
+        w_num = 0
+        for kaisai in kaisais:
+            for race in kaisai.races:
+                for horse in race.racehorses:
+                    oa = horse.result.order_of_arrival            
+                    if oa == 1:            
+                        self.t_icchaku[w_num,horse.num - 1] = 1
+                    elif oa == 2:
+                        self.t_nichaku[w_num,horse.num - 1] = 1
+                    elif oa == 3:
+                        self.t_sanchaku[w_num,horse.num - 1] = 1
+                    elif oa == 4:
+                        self.t_yonchaku[w_num,horse.num - 1] = 1
+                    elif oa == 5:
+                        self.t_gochaku[w_num,horse.num - 1] = 1        
+                w_num += 1
