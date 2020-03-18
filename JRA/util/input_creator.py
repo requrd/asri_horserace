e64676c5f5cd99f54100332fd1f8d145205934b3
Yuma Iwasaki <iwasaki@bioimpact.jp>
2020-03-18 14:12:39 +0900

    Apply black

--- JRA/util/input_creator.py
+++ JRA/util/input_creator.py
@@ -2,9 +2,10 @@
 import category_data
 import numpy as np
 
+
 class InputCreator:
     def __init__(self, kaisais):
         score_data = training_tool.createScoreDataMatrix(kaisais)
         sds = training_tool.standardize(score_data)
         cd = category_data.getCategoryData(kaisais)
-        self.x_data = np.concatenate((sds,cd),axis=2)
+        self.x_data = np.concatenate((sds, cd), axis=2)
